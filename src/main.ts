f174d6e017406a77d80bfc1c0f42852cf280f9c7
Maksim Maliukevich <maksim.maliukevich@intexsoft.by>
2019-05-31 16:12:20 +0300

    refactor package.json

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.enableCors();
   app.use(express.static('client/dist/client'));
   await app.listen(port);
 }
