40217c3b8ec9aebe2968104bcdbac633874f40d8
Maksim Maliukevich <maksim.maliukevich@intexsoft.by>
2019-05-31 15:38:08 +0300

    'refactor'

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import morgan = require('morgan');
+import * as express from 'express';
 
 const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+  app.use(express.static('client/dist/client'));
   await app.listen(port);
 }
 bootstrap();
